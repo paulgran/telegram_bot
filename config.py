13ac56ee35c341d8b8d93b8aa4b9cf148acf7bd7
paulgran <93615013+paulgran@users.noreply.github.com>
2025-06-18 16:24:18 +0700

    Add files via upload

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-GPT_API_KEY = os.getenv("GPT_API_KEY")
-GPT_MODEL = os.getenv("GPT_MODEL", "gpt-4")
+GPT_MODEL = "gpt-3.5-turbo"
